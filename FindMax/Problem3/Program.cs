ab4c83910179ef453b70fe06dc79d1169e3ad853
amymao64 <ymao64@gmail.com>
2017-01-15 22:05:46 -0600

    hw2

--- FindMax/Problem3/Program.cs
+++ FindMax/Problem3/Program.cs
@@ -24,6 +24,7 @@ namespace Problem3
             {
               
                 Console.Write(strArr[j] + "-");
+                
             }
             Console.Write(strArr[n]);
         }
